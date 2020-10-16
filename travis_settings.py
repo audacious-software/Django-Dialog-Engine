09084102799af4da4d64a4caac8409a9d3ee3a48
Chris J. Karr <chris@audacious-software.com>
2020-10-16 02:41:42 +0000

    Fixing MIDDLEWARE settings declaration

--- travis_settings.py
+++ travis_settings.py
@@ -29,13 +29,13 @@ INSTALLED_APPS = (
     'django_dialog_engine'
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
