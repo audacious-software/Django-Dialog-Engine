78b419b0125bd567ef25008240cf568234a14222
Chris J. Karr <chris@audacious-software.com>
2020-10-16 02:35:25 +0000

    Adding missing import, Python 2.7 link witelist

--- travis_urls.py
+++ travis_urls.py
@@ -12,6 +12,9 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
+import django
+
 from django.conf.urls import url
 
 urlpatterns = [
