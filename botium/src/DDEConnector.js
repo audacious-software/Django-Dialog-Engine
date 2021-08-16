f5c862caa9ea09cb3486b7d3ad0fb7450fe955fa
Chris J. Karr <chris@audacious-software.com>
2021-08-16 21:33:52 +0000

    Initial Botium dialog test pass

---
+++ botium/src/DDEConnector.js
@@ -0,0 +1,52 @@
+const { exec } = require("child_process");
+
+class DDEConnector {
+  constructor ({ queueBotSays, caps }) {
+    this.queueBotSays = queueBotSays
+    this.caps = caps
+  }
+
+  UserSays (msg) {
+    const me = this;
+    
+    const command_line = [
+        'python',
+        'manage.py',
+        'send_cli_message',
+        '"' + this.caps.DDE_DIALOG_USER_ID + '"',
+        '"' + this.caps.DDE_DIALOG_SCRIPT_PATH + '"',
+        '"' + msg['messageText'] + '"'
+    ];
+
+    console.log('Botium: %o', msg['messageText'].trim());
+
+    exec(command_line.join(' '), (error, stdout, stderr) => {
+        if (error) {
+            console.log('error: %o', error);
+            return;
+        }
+        
+        if (stderr) {
+            console.log('stderr: %o', stderr);
+            return;
+        }
+        
+        var lines = stdout.trim().split(/\r?\n/);
+        
+        for (var i = 0; i < lines.length; i++) {
+            var line = lines[i];
+            
+            console.log('Django Dialog Engine: %o', line);
+        }
+        
+        const botMsg = { messageText: stdout.trim() };
+
+        setTimeout(() => me.queueBotSays(botMsg), 0);
+    });
+  }
+}
+
+module.exports = {
+  PluginVersion: 1,
+  PluginClass: DDEConnector
+}
---
+++ management/commands/send_cli_message.py
@@ -0,0 +1,82 @@
+# pylint: disable=no-member, line-too-long
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+from builtins import str # pylint: disable=redefined-builtin
+from builtins import input # pylint: disable=redefined-builtin
+
+import json
+import os
+import signal
+import time
+
+from future import standard_library
+
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+from ...models import DialogScript, Dialog
+
+standard_library.install_aliases()
+
+class Command(BaseCommand):
+    help = 'Creates a new dialog from a provided JSON definition and executes it via the command line interface'
+
+    def add_arguments(self, parser):
+        parser.add_argument('dialog_user_id', type=str)
+        parser.add_argument('dialog_script_path', type=str)
+        parser.add_argument('message', type=str)
+
+    def handle(self, *args, **options):
+        dialog_user_id = options['dialog_user_id'];
+        dialog_script_path = options['dialog_script_path']
+
+        filename = os.path.basename(os.path.normpath(dialog_script_path))
+
+        key = filename + '-' + dialog_user_id
+
+        active_dialog = None
+
+        active_dialog = Dialog.objects.filter(key=key, finished=None).first()
+
+        if active_dialog is None:
+            dialog_script = json.load(open(dialog_script_path))
+
+            active_dialog = Dialog.objects.create(key=key, dialog_snapshot=dialog_script, started=timezone.now())
+
+        actions = active_dialog.process(options['message'])
+
+        if actions is None:
+            actions = []
+
+        for action in actions:
+            if action['type'] == 'echo':
+                print(action['message'])
+
+        last_transition = active_dialog.transitions.order_by('-when').first()
+
+        actions = active_dialog.process(None)
+
+        if actions is None:
+            actions = []
+
+        for action in actions:
+            if action['type'] == 'echo':
+                print(action['message'])
+
+        nudge_transition = active_dialog.transitions.order_by('-when').first()
+
+        while nudge_transition.pk != last_transition.pk:
+            last_transition = nudge_transition
+
+            actions = active_dialog.process(None)
+
+            if actions is None:
+                actions = []
+
+            for action in actions:
+                if action['type'] == 'echo':
+                    print(action['message'])
+
+            nudge_transition = active_dialog.transitions.order_by('-when').first()
