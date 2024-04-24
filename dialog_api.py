c988ce33058062d1a563b076fad763964bc6da07
Chris J. Karr <chris@audacious-software.com>
2024-04-24 15:23:14 -0500

    Code cleanup

--- dialog_api.py
+++ dialog_api.py
@@ -10,7 +10,7 @@ def identify_script_issues(script):
             node_name = node.get('name', None)
             node_id = node.get('id', None)
 
-            if len(actions) == 0:
+            if len(actions) == 0: # pylint: disable=len-as-condition
                 issues.append(('error', 'Random branch node "%s" (%s) has no configured actions.' % (node_name, node_id),))
             else:
                 for action in actions:
