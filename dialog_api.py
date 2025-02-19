f4ce49f874f930660357a05192898ae1033172cc
Chris J. Karr <chris@audacious-software.com>
2025-02-18 20:24:43 -0600

    Additional dialog issue testing

--- dialog_api.py
+++ dialog_api.py
@@ -1,9 +1,9 @@
 # pylint: disable=line-too-long
 
-def identify_script_issues(script):
+def identify_script_issues(script): # pylint: disable=too-many-branches
     issues = []
 
-    for node in script.definition:
+    for node in script.definition: # pylint: disable=too-many-nested-blocks
         if node['type'] == 'random-branch':
             actions = node.get('actions', [])
 
@@ -21,5 +21,28 @@ def identify_script_issues(script):
 
                     if destination == node_id:
                         issues.append(('error', 'Random branch node "%s" (%s) contains branch pointing back to itself.' % (node_name, node_id,),))
+
+        if node['type'] == 'branch-prompt':
+            node_id = node.get('id', None)
+            node_name = node.get('name', None)
+
+            timeout = node.get('timeout', None)
+
+            if timeout is not None:
+                timeout_node_id = node.get('timeout_node_id', None)
+
+                if timeout_node_id is None:
+                    issues.append(('error', 'Branching prompt node "%s" (%s) contains a timeout pointing to a null destination.' % (node_name, node_id,),))
+                else:
+                    found = False
+
+                    for test_node in script.definition:
+                        if timeout_node_id == test_node.get('id', None):
+                            found = True
+
+                            break
+
+                    if found is False:
+                        issues.append(('error', 'Branching prompt node "%s" (%s) contains a timeout pointing to a non-existent destination (%s).' % (node_name, node_id, timeout_node_id,),))
 
     return issues
