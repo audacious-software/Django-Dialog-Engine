a4ef1ac53d197d750c3deeeff981ed8530065cef
Chris J. Karr <chris@audacious-software.com>
2025-07-01 23:02:09 +0000

    Moved dialog stuff to builder package

--- dashboard_api.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse
-
-def dashboard_pages():
-    return [{
-        'title': 'Dialog Scripts',
-        'icon': 'speaker_notes',
-        'url': reverse('dashboard_dialog_scripts'),
-    }]
