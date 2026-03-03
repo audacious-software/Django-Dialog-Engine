7f557486c9170562fcc550cbafe6a10848b160a3
Chris J. Karr <chris@audacious-software.com>
2026-03-03 14:27:49 -0600

    Update grep script to use JSONPath instead of ObjectPath

--- management/commands/grep_dialog_scripts.py
+++ management/commands/grep_dialog_scripts.py
@@ -3,7 +3,8 @@
 
 import json
 
-import objectpath
+import jsonpath_ext as jsonpath
+
 import six
 
 from django.core.management.base import BaseCommand
@@ -11,23 +12,21 @@ from django.core.management.base import BaseCommand
 from ...models import DialogScript
 
 class Command(BaseCommand):
-    help = 'Queries DialogScript objects for nodes matching provided ObjectPath query. Reference: http://objectpath.org/reference.html'
+    help = 'Queries DialogScript objects for nodes matching provided JSONPatH query.'
 
     def add_arguments(self, parser):
         parser.add_argument('query', type=str)
 
     def handle(self, *args, **options): # pylint: disable=too-many-branches
-        query = options['query']
+        query = options['query'].replace('\\!', '!')
 
         scripts = DialogScript.objects.all()
 
         for script in scripts:
-            tree = objectpath.Tree(script.definition)
-
-            matches = list(tree.execute(query))
+            matches = jsonpath.find(query, script.definition)
 
             if matches:
                 six.print_('DialogScript: %s' % script.identifier)
 
                 for found in matches:
-                    six.print_('  %s' % json.dumps(found, indent=2))
+                    six.print_('  %s' % json.dumps(found.value, indent=2))
